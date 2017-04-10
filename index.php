a93bd4fc627c75cbaa48f1a4b98f03eebb5702b9
othelarian <le.maitre.killian@gmail.com>
2017-04-10 15:33:22 +0200

    end of dev on the eo server, close #11

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
 
 function connBdd() {
 	$create = !file_exists("journal.db");
-	$db = new PDO("sqlite:journal.db");
-	if ($create) {
+  try { $db = new PDO("sqlite:journal.db"); }
+  catch(PDOException $e) {
+    echo $e->getMessage();
+    return;
+  }
+  if ($create) {
 		$db->beginTransaction();
 		$db->exec("CREATE TABLE journal (date TIMESTAMP,idx TEXT,action TEXT,detail TEXT);");
 		$db->commit();
@@ -21,24 +25,39 @@ function connBdd() {
 function bddRequest($mod,$msg) {
 	if (!isset($_GET["arg"]) || !isset($_POST["detail"])) { echo $msg; return; }
 	$db = connBdd();
-	$db->beginTransaction();
-	$stmt = $db->prepare("INSERT INTO journal VALUES(CURRENT_TIMESTAMP,:idx,:type,:detail);");
-	$stmt->bindParam(":idx",$_GET["arg"]);
-	$stmt->bindParam(":type",$mod);
-	$stmt->bindParam(":detail",$_POST["detail"]);
-	$stmt->execute();
-	$db->commit();
+  try {
+    $db->beginTransaction();
+    $stmt = $db->prepare("INSERT INTO journal VALUES(CURRENT_TIMESTAMP,:idx,:type,:detail);");
+    $stmt->bindParam(":idx",$_GET["arg"]);
+    $stmt->bindParam(":type",$mod);
+    $stmt->bindParam(":detail",$_POST["detail"]);
+    $stmt->execute();
+    $db->commit();
+  }
+  catch(PDOException $e) {
+    echo $e->getMessage();
+  }
 }
 
 // récupération des données de synchro
 
-function setSynchro() {
-	if (!isset($_GET["arg"])) { echo "I need to sleep..."; return; }
+function getSynchro() {
+	if (!isset($_GET["arg"]) || !isset($_POST["detail"])) { echo "I need to sleep..."; return; }
 	$db = connBdd();
-	$bd->beginTransaction();
-	//
-	//
+  $arg = $_GET["arg"] . '%';
+	$db->beginTransaction();
+  $stmt = $db->prepare("SELECT * FROM journal WHERE idx NOT LIKE :idx AND date > :detail ORDER BY date;");
+  $stmt->bindParam(":idx",$arg);
+  $stmt->bindParam(":detail",$_POST["detail"]);
+  $stmt->execute();
+  $json = array('list' => array(),'last' => '0');
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+    $json['list'][] = $row['date'];
+    $json['last'] = $row['date'];
+    $json[$row['idx']] = array('idx'=>$row['idx'], 'action'=>$row['action'], 'detail'=>$row['detail']);
+  }
 	$db->commit();
+  echo json_encode($json);
 }
 
 // bad request
@@ -51,7 +70,7 @@ function badrequest() {
 
 if (isset($_GET["cmd"])) {
 	switch ($_GET["cmd"]) {
-		case "synchro": setSynchro(); break;
+		case "synchro": getSynchro(); break;
 		case "add": bddRequest("add","the debate turned mad."); break;
 		case "remove": bddRequest("rem","What's wrong with you?"); break;
 		case "mod": bddRequest("mod","I did it!"); break;
